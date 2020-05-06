b97ddbde9169702aa7051cc8068417b422ca1861
bvanh <48560384+bvanh@users.noreply.github.com>
2020-05-06 13:50:43 +0700

    fix track01

--- src/utils/submitEmail.js
+++ src/utils/submitEmail.js
@@ -16,7 +16,7 @@ function submitEmail(mailValue, phoneNumber) {
     .post(API.API_POST_EMAIL, QS.stringify(requestBody), config)
     .then(result => {
       if (result.status === 201) {
-        window.fbq('track', 'CompleteRegistration', { 'content_name': mailValue })
+        window.fbq('track', 'CompleteRegistration', { content_name: mailValue })
         return 201;
       }
     })
